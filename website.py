828e22d81a6efc8de4416b74d9dbf5f39a9ebd94
minheng <minhengwu@gmail.com>
2017-05-10 17:46:05 -0700

    flask website

--- website.py
+++ website.py
@@ -1,9 +1,12 @@
 from datetime import date
 from flask import Flask
-from utility import get_html
 app = Flask(__name__)
+
+
 def get_date():
-    return str(date.today().year) + '_' + str(date.today().month) + '_' + str(date.today().day)
+    return (str(date.today().year) + '_' + str(date.today().month) + '_' +
+            str(date.today().day))
+
 
 def get_html():
     a = get_date()
@@ -13,12 +16,14 @@ def get_html():
         <p>As of:%s</p>
         <img src="https://s3.amazonaws.com/hardoopmapreduce/%s.png">
       </body>
-    </html>''' % (a,a)
+    </html>''' % (a, a)
     return index_html
+
 
 @app.route('/')
 def main():
-   return get_html()
+    return get_html()
+
 
 if __name__ == '__main__':
-    app.run(host = "0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=80)
